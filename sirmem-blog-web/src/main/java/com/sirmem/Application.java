3816426034135f1d176d871f7fe9bb73b576ad7a
Needyou <needyous@outlook.com>
2025-07-27 22:20:14 +0800

    feat: 初始化工程项目

---
+++ sirmem-blog-web/src/main/java/com/sirmem/Application.java
@@ -0,0 +1,19 @@
+package com.sirmem;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@Configurable
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
